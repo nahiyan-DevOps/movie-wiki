02259adbeb3ee0525fc08ed9b3dcd3a9c7adae7a
Nahiyan <129832568+nahiyan-DevOps@users.noreply.github.com>
2023-06-19 10:47:42 +0600

    Update install.sh

--- shells/install.sh
+++ shells/install.sh
@@ -1,5 +1,10 @@
-sudo apt install python3-pip
-sudo apt install python3-virtualenv
-sudo virtualenv venv
+#!/bin/bash
+sudo apt update
+sudo apt install -y python3-pip python3-venv
+
+# Create and activate the virtual environment
+python3 -m venv venv
 source venv/bin/activate
-sudo pip3 install -r requirements.txt
+
+# Install dependencies
+pip install -r requirements.txt
