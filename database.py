5fce4ca35abfe0131da24c830e2c14c812f1dd79
Nahiyan <129832568+nahiyan-DevOps@users.noreply.github.com>
2023-06-19 10:39:25 +0600

    Update database.py

--- database.py
+++ database.py
@@ -5,11 +5,11 @@ import time
 time.sleep(5)  # Adjust the delay as needed
 # Database connection configuration
 db_config = {
-    "host": "movie-db",
+    "host": "nahiyan-pyapp-db.cpsvhbrundlq.eu-north-1.rds.amazonaws.com",
     "port": 3306,
     "user": "root",
-    "password": "123",
-    "database": "lalala",
+    "password": "bjit1234",
+    "database": "movieDB",
 }
 
 # Establish a database connection
@@ -20,7 +20,7 @@ def search_movies_by_year(year_of_release):
     db_connection = mysql.connector.connect(**db_config)  # Establish a new database connection
     db_cursor = db_connection.cursor()
 
-    query = "SELECT * FROM hahaha WHERE year_of_release = %s"
+    query = "SELECT * FROM hollywood WHERE year_of_release = %s"
     db_cursor.execute(query, (year_of_release,))
     results = db_cursor.fetchall()
 
@@ -31,7 +31,7 @@ def search_movies_by_year(year_of_release):
 
 
 def upload_movie_data(movie_name, year_of_release, box_office, director, producer, cast):
-    query = "INSERT INTO hahaha (movie_name, year_of_release, box_office, director, producer, cast) VALUES (%s, %s, %s, %s, %s, %s)"
+    query = "INSERT INTO hollywood (movie_name, year_of_release, box_office, director, producer, cast) VALUES (%s, %s, %s, %s, %s, %s)"
     values = (movie_name, year_of_release, box_office, director, producer, cast)
     db_cursor.execute(query, values)
     db_connection.commit()
